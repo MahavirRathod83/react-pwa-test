30bd66e31e08613587bbfc5ec21c347d4b0a1267
Mahavir <mahavirsinh.rathod@sayalife.in>
2024-09-03 16:29:38 +0530

    change

--- manifest.js
+++ manifest.js
@@ -2,16 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from "vite-plugin-pwa";
 
-const manifestForPlugIn: any = {
-    // add this to cache all the imports
-    workbox: {
-      globPatterns: ["**/*"],
-  },
-  // add this to cache all the
-  // static assets in the public folder
-  includeAssets: [
-      "**/*",
-  ],
+const manifestForPlugIn = {
+  registerType:'prompt',
+  includeAssests:['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
   manifest:{
     name:"React-vite-app",
     short_name:"react-vite-app",
